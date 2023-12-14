028705ffe1546658ea72123be77144eaa19c0bc5
ktbh4jc <ktbh4jc@gmail.com>
2023-12-14 13:59:55 -0600

    feat: add a bit more testing and docs. Test failing as method isn't implemented yet

--- 3/gear1.rb
+++ 3/gear1.rb
@@ -1,4 +1,47 @@
 # frozen_string_literal: true
 
+# A class that solves part 1 of https://adventofcode.com/2023/day/3
 class Gear1
+  def parce_input(input)
+    input.split("\n").map(&:strip)
+  end
+
+  # Where ABC Size can be a good metric in the long run, I find it cumbersome in smaller applications,
+  # especially at first. Given time I would want to clean this up more.
+  # rubocop:disable Metrics/AbcSize
+  def do_the_thing(input)
+    engine = parce_input(input)
+    sum = 0
+    (0..engine.size - 1).each do |row|
+      # Disableing the each/for preference so that I can edit column within the loop
+      # rubocop:disable Style/For
+      for column in 0..engine[row].size - 1
+        next unless engine[row][column].number?
+
+        full_number = find_full_number(engine, row, column)
+        neighbors = find_neighbors(engine, row, column, full_number)
+        skip_column_ahead_past_digit_count
+        sum += full_number if neighbors.has(symbols)
+      end
+      # rubocop:enable Style/For
+    end
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  # Given a parced engine diagram and the row and column of the start of a number, return the 
+  # full number. This method assumes that it always recieves the correct coordinates of the first
+  # digit of a number.
+  #
+  # @param engine [Array[String], #read] a representation of the provided engine
+  # @param row [Integer, #read] the row a given number exists on
+  # @param column [Integer, #read] the column a given number starts on
+  # @return [Integer] the full number starting at enging[row][collumn]
+  def find_full_number(engine, row, column) end
+end
+
+# Overwrite Integer to include a num_digits function (found on stackoverflow)
+class Integer
+  def num_digits
+    Math.log10(self).to_i + 1
+  end
 end
--- spec/3/gear1_spec.rb
+++ spec/3/gear1_spec.rb
@@ -4,7 +4,58 @@ require 'rspec'
 require './3/gear1'
 
 describe Gear1 do
+  before do
+    stub_const('TEST_INPUT',
+               "467..114..
+                ...*......
+                ..35..633.
+                ......#...
+                617*......
+                .....+.58.
+                ..592.....
+                ......755.
+                ...$.*....
+                .664.598..")
+    stub_const('TEST_INPUT_PARCED',
+               [
+                 '467..114..',
+                 '...*......',
+                 '..35..633.',
+                 '......#...',
+                 '617*......',
+                 '.....+.58.',
+                 '..592.....',
+                 '......755.',
+                 '...$.*....',
+                 '.664.598..'
+               ])
+  end
+
   it 'exists' do
     described_class.new
+  end
+
+  it 'properly parces a 2d input' do
+    gear1 = described_class.new
+    engine = gear1.parce_input(TEST_INPUT)
+    expect(engine).to eq(TEST_INPUT_PARCED)
+  end
+
+  describe '.find_full_number' do
+    it 'finds all numbers from test input' do
+      tests = { [0, 0] => 467 }
+      tests.each_key do |key|
+        expect(described_class.find_full_number(TEST_INPUT_PARCED, key[0], key[1])).to eq(tests[key])
+      end
+    end
+  end
+
+  describe Integer do
+    it 'correctly says how many digits a number has' do
+      tests = { 1 => 1, 10 => 2, 100 => 3, 1_000_000_000 => 10 }
+      tests.each_key do |key|
+        expect(key.num_digits).to eq(tests[key])
+      end
+    end
   end
 end
